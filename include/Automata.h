3111c7652d0e710cb1ec0d2fa4e452b6209e3eeb
HateStudy6 <115950271+HateStudy6@users.noreply.github.com>
2023-05-30 02:31:28 +0300

    dmitrieva

--- include/Automata.h
+++ include/Automata.h
@@ -1,6 +1,4 @@
-// Copyright 2022 GHA Test Team
-#ifndef INCLUDE_AUTOMATA_H_
-#define INCLUDE_AUTOMATA_H_
+
 
 #include <string>
 
@@ -20,7 +18,7 @@ std::string menu[7] = { "espresso",
  "cacao" };
 unsigned int prices[7] = {80, 90, 120, 130, 150, 100, 125};
 position state;
-unsigned int num_drink
+unsigned int num_drink;
 
  public:
 Automata();
@@ -38,4 +36,4 @@ void cook();
 void finish();
 void getChange(unsigned int price = 0);
 };
-#endif // INCLUDE_AUTOMATA_H_
+
