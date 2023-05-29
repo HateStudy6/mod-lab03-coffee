2c4e7bcfdc8e937af2ff06978b614899b8c83754
HateStudy6 <115950271+HateStudy6@users.noreply.github.com>
2023-05-30 02:10:52 +0300

    dmitrieva

--- include/Automata.h
+++ include/Automata.h
@@ -6,27 +6,27 @@
 #include <vector>
 
 enum position {off, wait, accept, check, cook};
- class Automata {
- private:
- unsigned int cash;
- std::string menu[7] = {"espresso", "americano", "cappuccino", "latte", "ice coffee", "tea", "cacao"};
- unsigned int prices[7] = {80, 90, 120, 130, 150, 100, 125};
- position state;
- unsigned int num_drink
- public:
-    Automata();
-    void on();
-    void off();
-    void coin(unsigned int summa);
-    void getMenu();
-    position getState();
-    void choice(unsigned int num);
-    bool check();
-    void cancel();
- private:
- void check();
- void cook();
- void finish();
- void getChange(unsigned int price=0);
+   class Automata {
+   private: 
+   unsigned int cash;
+   std::string menu[7] = {"espresso", "americano", "cappuccino", "latte", "ice coffee", "tea", "cacao"};
+   unsigned int prices[7] = {80, 90, 120, 130, 150, 100, 125};
+   position state;
+   unsigned int num_drink
+   public:
+   Automata();
+   void on();
+   void off();
+   void coin(unsigned int summa);
+   void getMenu();
+   position getState();
+   void choice(unsigned int num);
+   void check();
+   void cancel();
+   private:
+   void check();
+   void cook();
+   void finish();
+   void getChange(unsigned int price=0);
 };
 #endif // INCLUDE_AUTOMATA_H_
